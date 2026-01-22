0ff8e65c14c6f2aa0731799c5283e46478a99362
pope-h <hamatthan@gmail.com>
2026-01-22 20:01:02 +0100

    implement logic for notification module

---
+++ BackEnd/src/modules/notifications/notifications.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsService } from './notifications.service';
+import { Notification } from './entities/notification.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
+  providers: [NotificationsService],
+  exports: [NotificationsService],
+})
+export class NotificationsModule {}
